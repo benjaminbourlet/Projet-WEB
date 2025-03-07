aa6aa69b652c4c61e239b8a587f83b4ca7640714
T0wti <diego.borto31@gmail.com>
2025-03-07 15:00:10 +0100

    déplacement des boutons vers le bas

--- resources/views/account/users/info.blade.php
+++ resources/views/account/users/info.blade.php
@@ -5,25 +5,7 @@
 @section('content')
 
     <div class="max-w-md mx-auto mt-10 bg-white p-6 rounded-lg shadow-lg">
-        <!-- Boutons de modification et de suppression -->
-        <div class="mb-4 flex justify-between">
-            <!-- Bouton Modifier -->
-            <a href="{{ route('user_edit', ['role' => $role === 'Etudiant' ? 'students' : 'pilots', 'id' => $user->id]) }}"
-                class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
-                Modifier
-            </a>
-
-            <form action="{{ route('user_delete', ['id' => $user->id]) }}" method="POST"
-                onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer cette entreprise ?');">
-                @csrf
-                @method('DELETE')
-                <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600">
-                    Supprimer
-                </button>
-            </form>
-
-        </div>
-
+        
         <div class="container mx-auto p-4">
             <h1 class="text-2xl font-bold">Profil de {{ $user->name }}</h1>
 
@@ -79,6 +61,24 @@
                 <label class="block font-bold">Ville :</label>
                 <p class="border p-2 w-full bg-gray-100">{{ ($user->city)->name ?? 'Non défini' }}</p>
             </div>
+        </div>
+        <!-- Boutons de modification et de suppression -->
+        <div class="mb-4 flex justify-between">
+            <!-- Bouton Modifier -->
+            <a href="{{ route('user_edit', ['role' => $role === 'Etudiant' ? 'students' : 'pilots', 'id' => $user->id]) }}"
+                class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+                Modifier
+            </a>
+
+            <form action="{{ route('user_delete', ['id' => $user->id]) }}" method="POST"
+                onsubmit="return confirm('Êtes-vous sûr de vouloir supprimer cette entreprise ?');">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600">
+                    Supprimer
+                </button>
+            </form>
+
         </div>
     </div>
 
