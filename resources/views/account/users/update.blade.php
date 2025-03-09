868fffd21685315c46e2eb479e49319bbc28e873
T0wti <diego.borto31@gmail.com>
2025-03-09 15:23:31 +0100

    css de update fait, à confirmer avec les bros

--- resources/views/account/users/update.blade.php
+++ resources/views/account/users/update.blade.php
@@ -2,8 +2,10 @@
 
 @section('title', 'Informations de ' . $user->name)
 
-@section('content')
-    <div class="container mx-auto p-4">
+@include('partials.header')
+
+<main class="mt-10 mb-10 w-2/3 mx-auto">
+    <div class="container mx-auto p-4 bg-[#387077] rounded-lg shadow-lg">
         <h1 class="text-2xl font-bold">Profil de {{ $user->name }}</h1>
 
         <div class="mt-4">
@@ -13,34 +15,41 @@
         <form action="{{ route('user_update', ['role' => $role, 'id' => $user->id]) }}" method="POST"
             enctype="multipart/form-data">
             @csrf
+            <div class="grid grid-cols-1 place-items-center mt-1 w-full">
+                <label class="block font-bold mb-1">Photo de profil :</label>
+                <div class="relative w-full max-w-xs cursor-pointer">
+                    <input type="file" name="pp" class="absolute inset-0 w-full h-full opacity-0 cursor-pointer pointer-events-none">
+                    <div class="bg-[#D9D9D9] p-2 rounded-lg text-center">Choisir un fichier</div>
+                </div>
+            </div>
             <div>
                 <label class="block font-bold">Nom :</label>
-                <input type="text" name="name" value="{{ $user->name }}" class="border p-2 w-full">
+                <input type="text" name="name" value="{{ $user->name }}" class="border p-1.5 w-full bg-[#D9D9D9] rounded-lg text-lg"">
             </div>
             <div>
                 <label class="block font-bold">Prénom :</label>
-                <input type="text" name="first_name" value="{{ $user->first_name }}" class="border p-2 w-full">
+                <input type="text" name="first_name" value="{{ $user->first_name }}" class="border p-1.5 w-full bg-[#D9D9D9] rounded-lg text-lg">
             </div>
 
             <div class="mb-4">
                 <label class="block font-bold">Date de naissance :</label>
-                <input type="date" name="birthdate" value="{{ $user->birthdate ? \Carbon\Carbon::parse($user->birthdate)->format('Y-m-d') : '' }}" class="border p-2 w-full">
+                <input type="date" name="birthdate" value="{{ $user->birthdate ? \Carbon\Carbon::parse($user->birthdate)->format('Y-m-d') : '' }}" class="border p-1.5 w-full bg-[#D9D9D9] rounded-lg text-lg">
             </div>
 
             <div>
                 <label class="block font-bold">Email :</label>
-                <input type="email" name="email" value="{{ $user->email }}" class="border p-2 w-full">
+                <input type="email" name="email" value="{{ $user->email }}" class="border p-1.5 w-full bg-[#D9D9D9] rounded-lg text-lg">
             </div>
 
             <div class="mb-4">
                 <label class="block font-bold">Région :</label>
-                <p class="border p-2 w-full bg-gray-100">{{ optional(optional($user->city)->region)->name ?? 'Non défini' }}
+                <p class="border p-1.5 w-full bg-[#D9D9D9] rounded-lg text-lg">{{ optional(optional($user->city)->region)->name ?? 'Non défini' }}
                 </p>
             </div>
 
             <div class="mb-4">
                 <label class="block font-bold">Ville :</label>
-                <select name="city_id" class="border p-2 w-full">
+                <select name="city_id" class="border p-1.5 w-full bg-[#D9D9D9] rounded-lg text-lg">
                     <option value="">Sélectionnez une ville</option>
                     @foreach($cities as $city)
                         <option value="{{ $city->id }}" {{ $user->city_id == $city->id ? 'selected' : '' }}>
@@ -52,8 +61,8 @@
 
             @if ($role === 'Etudiant')
                 <div>
-                    <label for="classe_id" class="block text-sm font-medium text-gray-700">Classe (Promo)</label>
-                    <select id="classe_id" name="classe_id" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
+                    <label for="classe_id" class="block font-bold">Classe (Promo)</label>
+                    <select id="classe_id" name="classe_id" class="mt-1 block w-full border-gray-300 rounded-md shadow-sm bg-[#D9D9D9]">
                         <option value="">Sélectionnez une classe</option>
                         @foreach($classes as $classe)
                             <option value="{{ $classe->id}}" {{ $user->classe_id == $classe->id ? 'selected' : '' }}>
@@ -63,10 +72,10 @@
                 </div>
             @elseif ($role === 'Pilote')
                 <div>
-                    <label for="classesPilots" class="block text-sm font-medium text-gray-700">Classes (Sélection
+                    <label for="classesPilots" class="block font-bold">Classes (Sélection
                         multiple)</label>
                     <select id="classesPilots" name="classesPilots[]" multiple
-                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm">
+                        class="mt-1 block w-full border-gray-300 rounded-md shadow-sm bg-[#D9D9D9]">
                         @foreach($classes as $classe)
                             <option value="{{ $classe->id }}" {{ in_array($classe->id, $user->classesPilots->pluck('id')->toArray()) ? 'selected' : '' }}>
                                 {{ $classe->name }}
@@ -78,18 +87,20 @@
 
 
             <div>
-                <label for="new_classe" class="block text-sm font-medium text-gray-700">Ou créez une nouvelle classe</label>
-                <input type="text" id="new_classe" name="new_classe" class="w-full mt-1 p-2 border rounded-md">
+                <label for="new_classe" class="block text-xs font-bold">Ou créez une nouvelle classe</label>
+                <input type="text" id="new_classe" name="new_classe" class="w-full border p-1.5 bg-[#D9D9D9] rounded-lg text-xs">
             </div>
 
-            <div>
-                <label class="block font-bold">Photo de profil :</label>
-                <input type="file" name="pp" class="border p-2 w-full">
+            <div class="flex justify-between">
+                <button type="button" class="bg-[#3D9DA9] hover:bg-[#3D8A8F] text-red-700 px-4 py-2 rounded mt-4" onclick="window.history.back()">
+                    Annuler
+                </button>
+                <button type="submit" class="bg-[#3D9DA9] hover:bg-[#3D8A8F] text-white px-4 py-2 rounded mt-4">
+                    Modifier
+                </button>
             </div>
-
-            <button type="submit" class="bg-teal-500 hover:bg-teal-600 text-white px-4 py-2 rounded">
-                Modifier
-            </button>
         </form>
     </div>
-@endsection
+</main>
+
+@include('partials.footer')
