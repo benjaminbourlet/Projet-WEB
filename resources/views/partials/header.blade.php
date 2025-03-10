1a05564f0bccdb1edfb56540af17cc22405b2770
Bourlet Benjamin <benjamin.bourlet@icloud.com>
2025-03-10 15:46:07 +0100

    Header : V-10_03_2025

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,33 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Header page</title>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.5.3/flowbite.min.js"></script>
-</head>
+@extends('layouts.app')
 
 <body>
     <nav class="bg-[#5A8E95] border-gray-200">
         <div class="max-w-100 h-20 flex justify-between items-center mx-6 py-0">
             <!-- Logo + Texte -->
-            <div class="flex items-center space-x-3 rtl:space-x-reverse">
-            <a href="{{ route('home') }}">
-                <button>
-                    <img src="{{ asset('storage/images/logo.png') }}" alt="Logo" class="w-18 h-16">
-                </button>
-            </a>
-            <span class="self-center text-2xl font-semibold whitespace-nowrap">Cesi ton stage</span>
+            <div class="flex items-center self-center space-x-3 rtl:space-x-reverse z-10">
+                <a href="{{ route('home') }}">
+                    <button>
+                        <img src="{{ asset('storage/images/Logo.png') }}" alt="Logo" width="80" height="80">
+                    </button>
+                </a>
+                <a href="{{ route('home') }}">
+                <span class="self-center text-2xl font-semibold whitespace-nowrap">Cesi ton stage</span>
+                </a>
             </div>
 
             <!-- Menu central -->
-            <div class="items-center justify-between hidden md:flex md:w-auto " id="navbar-user">
+            <div class="flex justify-between hidden md:flex md:w-auto " id="navbar-user">
                 <ul class="flex flex-col font-medium text-2xl p-4 md:p-0 mt-4 border md:space-x-20 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  ">
-                <li><a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Accueil</a></li>
-                    <li><a href="{{ route('offer_list') }}" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Offres</a></li>
-                    <li><a href="{{ route('company_list') }}" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Entreprises</a></li>
+                    <li><a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Acceuil</a></li>
+                    <li><a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Offres</a></li>
+                    <li><a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Entreprises</a></li>
                     <li><a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Avis</a></li>
                 </ul>
             </div>
@@ -41,11 +34,11 @@
                         </button>
                         <div class="py-6 px-6 lg:px-8">
                             <div class="items-center justify-between bg-white md:flex md:w-auto md:order-1" id="navbar-user">
-                                <ul class="flex flex-col font-medium text-2xl p-4 md:p-0 mt-4 border md:space-x-20 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-                                    <li><a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Accueil</a></li>
-                                    <li><a href="{{ route('offer_list') }}" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Offres</a></li>
-                                    <li><a href="{{ route('company_list') }}" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Entreprises</a></li>
-                                    <li><a href="#" class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-white md:p-0">Avis</a></li>
+                                <ul class="flex flex-col font-medium text-2xl text-center p-4 md:p-0 mt-4 border md:space-x-20 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+                                    <li><a href="#" class="block py-2 px-3 text-black rounded-sm hover:bg-gray-200 md:hover:bg-transparent md:hover:text-white md:p-0">Accueil</a></li>
+                                    <li><a href="#" class="block py-2 px-3 text-black rounded-sm hover:bg-gray-200 md:hover:bg-transparent md:hover:text-white md:p-0">Offres</a></li>
+                                    <li><a href="#" class="block py-2 px-3 text-black rounded-sm hover:bg-gray-200 md:hover:bg-transparent md:hover:text-white md:p-0">Entreprises</a></li>
+                                    <li><a href="#" class="block py-2 px-3 text-black rounded-sm hover:bg-gray-200 md:hover:bg-transparent md:hover:text-white md:p-0">Avis</a></li>
                                 </ul>
                             </div>
                         </div>
@@ -56,19 +49,11 @@
 
             <!-- User Account à droite -->
             <div class="flex items-center space-x-3">
-            @auth
-                <!-- Affiche la photo de profil si l'utilisateur est connecté -->
-                <button type="button" class="flex text-sm rounded-full focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-                    <img class="rounded-full" src="{{ asset('storage/' . auth()->user()->pp_path) }}" alt="user photo" width="50" height="50">
-                </button>
-                @else
-                <!-- Affiche l'icône de connexion si l'utilisateur n'est pas connecté -->
+                @auth
                 <button type="button" class="flex text-sm rounded-full focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                     <img class="rounded-full" src="storage/images/icon_login.jpeg" alt="user photo" width="50" height="50">
                 </button>
-                @endauth
                 <!-- Dropdown menu -->
-                @auth
                 <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow-sm overflow-hidden" id="user-dropdown">
                     <ul class="py-2" aria-labelledby="user-menu-button">
                         <li><a href="{{ route('dashboard') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Dashboard</a></li>
@@ -99,6 +84,9 @@
                 </div>
                 @endauth
 
+                <button type="button" class="flex text-sm rounded-full focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+                    <img class="rounded-full" src="storage/images/icon_login.jpeg" alt="user photo" width="50" height="50">
+                </button>
 
                 <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="btn md:hidden">
                     <img src="{{ asset('storage/images/burger_menu.svg') }}" alt="Menu burger" width="35" height="35">
@@ -109,5 +97,3 @@
         </div>
     </nav>
 </body>
-
-</html>
